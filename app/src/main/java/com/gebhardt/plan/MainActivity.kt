5b5be114d51b865638315292298810af87a86438
Robert <robert.gol.jr@gmail.com>
2017-07-16 13:26:45 -0300

    fragment list sample

--- app/src/main/java/com/gebhardt/plan/MainActivity.kt
+++ app/src/main/java/com/gebhardt/plan/MainActivity.kt
@@ -2,12 +2,20 @@ package com.gebhardt.plan
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.gebhardt.plan.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_item_list)
         supportActionBar!!.hide()
+        val mRecyclerView = findViewById<RecyclerView>(R.id.list)
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        mRecyclerView.adapter = MyItemRecyclerViewAdapter(DummyContent.ITEMS, null)
     }
+
 }
